00c00712acc477cfc8ce01289b1ce1fd7f8ee209
sayali patil <patilsayali2@gmail.com>
2023-09-06 02:19:46 +0530

    logging and exception

---
+++ src/logger.py
@@ -0,0 +1,18 @@
+# used so that we can log and track any errors/exception 
+
+
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE) #whatever logs will get created will be here
+os.makedirs(logs_path,exist_ok=True)   #keep appending files here
+
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
